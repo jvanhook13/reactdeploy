6d12eca715071446259d79f18b3b128085789550
jvanhook13 <jaredvanhook1313@gmail.com>
2020-06-03 21:34:16 -0700

    added all files

---
+++ client/build/precache-manifest.fa78bfea17ebf50255d73bcaf81debde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8eda4583c828ef32ca1",
+    "url": "/static/css/main.cda0e815.chunk.css"
+  },
+  {
+    "revision": "b8eda4583c828ef32ca1",
+    "url": "/static/js/main.edf1b0fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219f29db5f10382fa35f",
+    "url": "/static/js/2.600514d9.chunk.js"
+  },
+  {
+    "revision": "7ea4e265761ead42247416c4f1cd2cc2",
+    "url": "/index.html"
+  }
+];
